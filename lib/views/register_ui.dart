993191be8caa48f926caf70c12a3bd9e5520e59f
Intorn Rungroadsawakun <s6552410027@sau.ac.th>
2024-03-03 12:17:09 +0700

    register

--- lib/views/register_ui.dart
+++ lib/views/register_ui.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,6 +11,7 @@ class RegisterUI extends StatefulWidget {
 }
 
 class _RegisterUIState extends State<RegisterUI> {
+  bool pwdShow = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +32,165 @@ class _RegisterUIState extends State<RegisterUI> {
           Icon(
             Icons.arrow_back_ios,
             color: Colors.white,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            right: MediaQuery.of(context).size.width * 0.1,
+            left: MediaQuery.of(context).size.width * 0.1,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: MediaQuery.of(context).size.width * 0.5,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Text(
+                  'เข้าใช้งานแอพฃปพลิเคชั่น',
+                  style: GoogleFonts.kanit(
+                    textStyle: TextStyle(
+                      color: const Color.fromARGB(255, 134, 13, 5),
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.width * 0.04,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                    hintText: 'username',
+                    labelText: 'ชื่อผู้ใช้',
+                    hintStyle: GoogleFonts.kanit(),
+                    labelStyle: GoogleFonts.kanit(
+                      textStyle: TextStyle(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                TextField(
+                  obscureText: pwdShow,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                    hintText: 'password',
+                    labelText: 'รหัสผ่าน',
+                    hintStyle: GoogleFonts.kanit(),
+                    labelStyle: GoogleFonts.kanit(
+                      textStyle: TextStyle(
+                        color: const Color.fromARGB(255, 134, 13, 5),
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (pwdShow == true) {
+                            pwdShow = false;
+                          } else {
+                            pwdShow = true;
+                          }
+                        });
+                      },
+                      icon: Icon(
+                        //ternary operator --> ___?____:_____
+                        pwdShow ? Icons.visibility_off : Icons.visibility,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Log in',
+                    style: GoogleFonts.kanit(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(MediaQuery.of(context).size.width,
+                        MediaQuery.of(context).size.height * 0.07),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 134, 13, 5),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Do not have account?  ',
+                      style: GoogleFonts.kanit(
+                        textStyle: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterUI(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Register',
+                        style: GoogleFonts.kanit(
+                          textStyle: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height * 0.02,
+                            color: const Color.fromARGB(255, 134, 13, 5),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
